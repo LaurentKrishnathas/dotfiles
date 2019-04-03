0df187bac1787b22e0946d862e5db58e56567c8f
lk <lk@dotmatics.com>
2019-04-03 11:33:55 +0100

    more

--- bash/functions/terraform.bash
+++ bash/functions/terraform.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function terraform_project_init {
-	for file in terraform/live/sample/test/*
+	for file in $DOTFILES_DIR/infra/terraform/live/sample/test/*
 	do
             name=${file##*/}
 			echo "copying file $file  to ${name}.TODO"
