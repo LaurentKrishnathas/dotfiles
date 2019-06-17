170f97f4c35fb25cbca766bc1341cb232ee91d99
lk <lk@dotmatics.com>
2019-06-17 07:27:12 +0100

    created functions to use code completion for aws, make, terraform

---
+++ bash/functions/aws-vault.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# @author Laurent Krishnathas
+# @year 2019
+
+
+function aws-vault-helper {
+    AWS_VAULT=${AWS_VAULT:-""}
+    CMD_=$1
+
+    if [ -z "$AWS_DEFAULT_PROFILE" ]
+    then
+        echo "Warning AWS_DEFAULT_PROFILE is not set "
+        return
+    fi
+
+    if [ -z "$AWS_VAULT" ]
+    then
+        shift
+        exec aws-vault exec "${AWS_DEFAULT_PROFILE:-work}" -- $CMD_ "$@"
+    else
+        shift
+        $CMD_ "$@"
+    fi
+}
+
+function aws {
+    aws-vault-helper /Users/lk/Library/Python/3.7/bin/aws $@
+}
+
+function terraform {
+    aws-vault-helper /usr/local/bin/terraform $@
+}
+
+function make {
+    aws-vault-helper /usr/bin/make $@
+}
--- bash/functions/aws.bash
+++ bash/functions/aws.bash
@@ -10,27 +10,3 @@ function ssm {		#... .restartWifi
 }
 
 
-# Bad idea as sub process can steal env variables, so we need to use /> aws-vault exec profilename  -- command
-# to limit exposure to anything other than the intended process
-
-#
-#function switchAWSProfile {
-#    unsetAWS
-#    aws-vault list
-#    PROFILE=$1
-#    echo "trying to swtich to $PROFILE ..."
-#    eval "$(aws-vault  exec $PROFILE  -- env|grep AWS|sed 's/^/export /' )"
-#    echo "checking identitty ..."
-#    aws sts get-caller-identity || echo "maybe sts permission missing ?"
-#    echo "switched to seesion ..."
-#}
-#
-#function unsetAWS {
-#    echo "unsetting all AWS* env"
-#    unset AWS_VAULT
-#    unset AWS_DEFAULT_REGION
-#    unset AWS_ACCESS_KEY_ID
-#    unset AWS_SECRET_ACCESS_KEY
-#    unset AWS_SESSION_TOKEN
-#    unset AWS_SECURITY_TOKEN
-#}
